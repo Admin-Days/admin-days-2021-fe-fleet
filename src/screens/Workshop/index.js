57ffee45460ba35fc618b28ef7becee35d520eff
ganiilhamirsyadi@gmail.com <ganiilhamirsyadi@gmail.com>
2021-10-28 15:32:47 +0700

    remove unused import

--- src/screens/Workshop/index.js
+++ src/screens/Workshop/index.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Main from "./Main";
 import Speaker from "./Speaker";
-import styles from "./Workshop.module.sass";
 
 const Workshop = () => {
   return (
